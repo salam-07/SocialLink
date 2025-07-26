9aad363d6092cc7a3ea17abf0dbf67eaeffb8bdd
Abdus Salam Ameen <abdussalamblue7@gmail.com>
2025-07-26 15:45:17 +0500

    Add landing page

--- sociallinkapp/routes.py
+++ sociallinkapp/routes.py
@@ -7,8 +7,7 @@ from flask import render_template, url_for, redirect, flash, request, abort
 from sociallinkapp import app
 
 
-# route for the home page
+# route for the landing page
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('index.html')
+def landing():
+    return render_template('landing.html')
