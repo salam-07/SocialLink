92176d22f6e3dd940c54716486ffeda3fc736e4c
Abdus Salam Ameen <abdussalamblue7@gmail.com>
2025-07-26 15:38:11 +0500

    Add initial layout and home page templates

--- sociallinkapp/routes.py
+++ sociallinkapp/routes.py
@@ -1,6 +1,13 @@
+# contains all the routes needed for application
+
+# libraries import
 from flask import render_template, url_for, redirect, flash, request, abort
+
+# local imports
 from sociallinkapp import app
 
+
+# route for the home page
 @app.route('/')
 @app.route('/home')
 def home():
