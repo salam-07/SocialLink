48d9ace92bb33b999edc091eaf2bf0ca21a60e37
salam-07 <abdussalamblue7@gmail.com>
2025-07-29 18:15:52 +0500

    Add URL Support for platforms

--- sociallinkapp/models.py
+++ sociallinkapp/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from sociallinkapp import db
 import uuid
+import json
 
 class Post(db.Model):
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
@@ -10,12 +11,19 @@ class Post(db.Model):
     platforms = db.Column(db.Text, nullable=False)  # JSON string of selected platforms
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
+    # Platform URLs - can be empty if not posted to that platform
+    linkedin_url = db.Column(db.Text, nullable=True)
+    facebook_url = db.Column(db.Text, nullable=True)
+    twitter_url = db.Column(db.Text, nullable=True)
+    instagram_url = db.Column(db.Text, nullable=True)
+    reddit_url = db.Column(db.Text, nullable=True)
+    threads_url = db.Column(db.Text, nullable=True)
+    
     def __repr__(self):
         return f"Post(id='{self.id}', type='{self.post_type}', date='{self.date_posted}')"
     
     def get_platforms_list(self):
         """Convert platforms JSON string back to list"""
-        import json
         try:
             return json.loads(self.platforms)
         except:
@@ -23,16 +31,32 @@ class Post(db.Model):
     
     def set_platforms_list(self, platforms_list):
         """Convert platforms list to JSON string for storage"""
-        import json
         self.platforms = json.dumps(platforms_list)
-    def get_platforms_list(self):
-        import json
-        try:
-            return json.loads(self.platforms)
-        except:
-            return []
     
-    # convert list to JSON string
-    def set_platforms_list(self, platforms_list):
-        import json
-        self.platforms = json.dumps(platforms_list)
+    def get_platform_url(self, platform):
+        """Get the URL for a specific platform"""
+        platform_mapping = {
+            'linkedin': self.linkedin_url,
+            'facebook': self.facebook_url,
+            'twitter': self.twitter_url,
+            'x': self.twitter_url,  # X is the same as Twitter
+            'instagram': self.instagram_url,
+            'reddit': self.reddit_url,
+            'threads': self.threads_url
+        }
+        return platform_mapping.get(platform) or '#'
+    
+    def set_platform_url(self, platform, url):
+        """Set the URL for a specific platform"""
+        if platform == 'linkedin':
+            self.linkedin_url = url
+        elif platform == 'facebook':
+            self.facebook_url = url
+        elif platform in ['twitter', 'x']:
+            self.twitter_url = url
+        elif platform == 'instagram':
+            self.instagram_url = url
+        elif platform == 'reddit':
+            self.reddit_url = url
+        elif platform == 'threads':
+            self.threads_url = url
